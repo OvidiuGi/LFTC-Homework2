37eca6d39ddd91c60b2022868a2e71c258b3e5eb
OvidiuGi <gireada_ovidiu@yahoo.com>
2022-11-11 09:41:44 +0200

    6

--- app/code/main.php
+++ app/code/main.php
@@ -9,13 +9,10 @@ $errorTableFile = fopen('errorTable.txt', 'w');
 
 $lexicalScanner = new LexicalScanner();
 
-//Inside the LexicalScanner class, set the input from file.txt
 $lexicalScanner->setInput($file);
-//with the input from file.txt, the function forms the SymbolTable array,
-//the PIF array and Error array
-//Writes in a file the Symbol table
+
 $lexicalScanner->getSymbolTableFile($symbolTableFile);
-//Writes in a file the PIF table
+
 $lexicalScanner->getProgramInternalFormFile($programInternalFormFile);
-//Writes in a file the Error table
+
 $lexicalScanner->getErrorFile($errorTableFile, $file);
